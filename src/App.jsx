2e8050d0d5815b062fdc533fcc08fad2559fd213
varadisthedev <varadisthedev@gmail.com>
2025-09-11 00:22:22 +0530

    Setting up: React+Vite , a 'Hello world' from react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World 🚀</h1>
+      <p>React + Vite is working!</p>
+
+      <h1>react has sooo many files!</h1>
+      <p>it looks soo complicated, i hope i master it one day</p>
+    </div>
+  );
+}
