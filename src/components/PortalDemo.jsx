581d11c30501df423367a494c02439227b140c31
varadisthedev <varadisthedev@gmail.com>
2025-09-23 23:15:15 +0530

    add portals in react

---
+++ src/components/PortalDemo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function PortalDemo() {
+  return(
+    <div>
+      <h1>Portals Demo</h1>
+    </div>
+    // document.getElementById('portal-root')
+  );
+}
+
+export default PortalDemo;
